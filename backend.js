c4360e8758c476dd6e1e7c95e465f0249ef1ac90
chunzkok <kokcz@comp.nus.edu.sg>
2023-03-26 19:54:31 +0800

    Fix home.html and point backend to modify home.html

--- backend.js
+++ backend.js
@@ -1,70 +1,70 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/style') {
-    const cssFilePath = path.join(__dirname, 'style.css');
+  if (req.method === "GET" && req.url === "/style") {
+    const cssFilePath = path.join(__dirname, "style.css");
 
     fs.readFile(cssFilePath, (err, data) => {
       if (err) {
         res.statusCode = 500;
         res.end(`Error getting the file: ${err}.`);
       } else {
-        res.setHeader('Content-Type', 'text/css');
+        res.setHeader("Content-Type", "text/css");
         res.end(data);
       }
     });
-  } else if (req.method === 'GET' && req.url === '/html') {
-    const cssFilePath = path.join(__dirname, 'src/index.html');
+  } else if (req.method === "GET" && req.url === "/html") {
+    const cssFilePath = path.join(__dirname, "src/index.html");
 
     fs.readFile(cssFilePath, (err, data) => {
       if (err) {
         res.statusCode = 500;
         res.end(`Error getting the file: ${err}.`);
       } else {
-        res.setHeader('Content-Type', 'text/css');
+        res.setHeader("Content-Type", "text/css");
         res.end(data);
       }
     });
-  } else if (req.method === 'POST' && req.url === '/uploadcss') {
-    let body = '';
-    req.on('data', (chunk) => {
+  } else if (req.method === "POST" && req.url === "/uploadcss") {
+    let body = "";
+    req.on("data", (chunk) => {
       body += chunk;
     });
-    req.on('end', () => {
-      const cssFilePath = path.join(__dirname, 'style.css');
+    req.on("end", () => {
+      const cssFilePath = path.join(__dirname, "style.css");
       fs.writeFile(cssFilePath, body, (err) => {
         if (err) {
           res.statusCode = 500;
           res.end(`Error saving the file: ${err}.`);
         } else {
           res.statusCode = 200;
-          res.end('File uploaded successfully.');
+          res.end("File uploaded successfully.");
         }
       });
     });
-  } else if (req.method === 'POST' && req.url === '/uploadhtml') {
-    let body = '';
-    req.on('data', (chunk) => {
+  } else if (req.method === "POST" && req.url === "/uploadhtml") {
+    let body = "";
+    req.on("data", (chunk) => {
       body += chunk;
     });
 
-    req.on('end', () => {
-      const cssFilePath = path.join(__dirname, 'src/index.html');
+    req.on("end", () => {
+      const cssFilePath = path.join(__dirname, "./home.html");
       fs.writeFile(cssFilePath, body, (err) => {
         if (err) {
           res.statusCode = 500;
           res.end(`Error saving the file: ${err}.`);
         } else {
           res.statusCode = 200;
-          res.end('File uploaded successfully.');
+          res.end("File uploaded successfully.");
         }
       });
     });
   } else {
     res.statusCode = 404;
-    res.end('Not found');
+    res.end("Not found");
   }
 });
 
