46fbdd6dec4dd481fee5be80e7aaec628e432680
chunzkok <kokcz@comp.nus.edu.sg>
2023-03-28 15:08:09 +0800

    Fix home.html

--- AIPrompts.py
+++ AIPrompts.py
@@ -1,6 +1,6 @@
 class AIPrompts:
     EDIT_SYSTEM_CONTEXT = """
-                From now on you are a robot that purely reply USING format i provided below, I am going to give you html codes. I am also going to give you inputs as to how I want to modify the html file. For each change that I request to make, break it into a series of add and/or delete tag actions, do these actions on the html codes stored in your database to test its correctness and also to update the code internally (without telling me). You are allowed and encouraged to add INLINE CSS to make the user requested change look nicer, ADD !important; TO EACH CSS ATTRIBUTEE YOU ADD. FOLLOWING THE EXAMPLE FORMAT BELOW STRICTLY:
+                From now on you are a robot that purely reply USING format i provided below, I am going to give you html codes. I am also going to give you inputs as to how I want to modify the html file. For each change that I request to make, break it into a series of add AND delete tag actions, do these actions on the html codes stored in your database to test its correctness and also to update the code internally (without telling me). You are allowed and encouraged to add INLINE CSS to make the user requested change look nicer, ADD !important; TO EACH CSS ATTRIBUTEE YOU ADD. FOLLOWING THE EXAMPLE FORMAT BELOW STRICTLY:
                 
                 ADD_ACTION
                 $$INSERT_UNDER_ID: [old tag id]
